4ce6e69971da7394eba224f64fb366a2f0e17d4b
aanahitt1 <aanahitt@hotmail.com>
2019-11-26 14:19:24 -0800

    Add files via upload

--- parsexml.cpp
+++ parsexml.cpp
@@ -44,6 +44,42 @@ ParseXML::ParseXML()
 
         }
 
+        //This function will return a string of descriptions from an XML document
+        QString* getDescriptions(QString FileName) {
+            //The QDomDocument class represents an XML document.
+            QDomDocument xmlBOM;
+            // Load xml file as raw data
+            QFile f(FileName);
+            if (!f.open(QIODevice::ReadOnly ))
+            {
+                // Error while loading file
+                QString *error = new QString();
+                QString message = "Error while loading file.";
+                error[0] = message;
+                return error;
+            }
+            // Set data into the QDomDocument before processing
+            xmlBOM.setContent(&f);
+            f.close();
+
+            //Find the names and store them
+            QDomElement program = xmlBOM.documentElement().firstChild().toElement();
+            QString* names = new QString();
+            int i = 0;
+
+            while(!program.isNull()) {
+                if(program.tagName() == "Program") {
+                    QString name = program.attribute("desc");
+                    names[i] = name;
+                    i++;
+                }
+            }
+
+            return names;
+
+        }
+
+
     };
 
 }
