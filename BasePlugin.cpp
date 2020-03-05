7335dc27de15f5b655545c787dcbe40fbaa9cc89
CptnYankee <jjstanderfer@gmail.com>
2020-03-05 12:22:09 -0800

    Updated to impletement most functions

--- BasePlugin.cpp
+++ BasePlugin.cpp
@@ -1,10 +1,4 @@
-#include "IAlgorithm.h"
 #include "BasePlugin.h"
-#include <QObject>
-#include <QProcess>
-#include <QString>
-#include <QJsonArray>
-#include <QVariant>
 
 #ifdef Q_OS_WINDOWS
 #define MYEXPORT __declspec(dllexport)
@@ -17,45 +11,39 @@ Q_INTERFACES(IAlgorithm)
 
 BasePlugin::BasePlugin(){
     //placeholder, generalization needed
-    initiate("RNAfold.exe", {"--noPS"});
+    //initiate("RNAfold.exe", );
 }
 
-BasePlugin::BasePlugin(QJsonObject ini){
-    QString str = "options";
+BasePlugin::BasePlugin(QString path){
 
-    //ini is the entirety of the .json file for this plugin
-    //It can be converted into a QVariantMap
-     pluginMap = new QVariantMap(ini.toVariantMap());
-    //Supposedly a QVariantMap is "Synonym for QMap<QString, QVariant>."
-    //Whatever that means...
-     //from pluginMap, which is declared as QMap<QString,QVariant>, we should be able to extract values by using
-     //pluginMap->value()/pluginMap->key() to return the QVariant/QString
-     //this however, returns a const object. which we can't assign to anything
-     //instead we have to do this
-
-     //To-do: Initialize using total number of unique options for each plugin
-     QString commandArr[50];
-     //QStringList optionArr[50];
-
-     QVariant var;
-     var = pluginMap->value("options");
-
-     QSequentialIterable itrVar = var.value<QSequentialIterable>();
-
-     int i = 0;
-     for(const QVariant &v : itrVar){
-        QMap<QString,QVariant> tempMap;
-        tempMap = v.toMap();
-        commandArr[i] = tempMap.value("command").toString();
-        map.unite(tempMap.value("pass").value<QMap<QString,double>>());
-     }
+    findDir(path);
+    findJson();
+    extractJsonData();
 }
 
-void BasePlugin::initiate(QString filepath, QStringList opt){
+//From a filepath with the .exe location and a list of options, construct and pass a command line argument.
+QString BasePlugin::initiate(QString filepath, QMap<QString, double> opt){
     file_path = filepath;
-   // proc = new QProcess(this);
+    proc = new QProcess(NULL);
 
+    QStringList args = uniteMaps(opt);
+
+    args << "-i " + filepath;
+
+    //@To-do:
+    //Set up check for exe location
+    proc->setProgram("RNAfold.exe");
+    proc->setArguments(args);
+
+    proc->start();
+    proc->waitForFinished();
+    structure = proc->readAllStandardOutput();
+    QString err = proc->readAllStandardError();
+
+    return structure;
 }
+
+
 
 QString BasePlugin::getStructure(){
     return structure;
@@ -65,6 +53,74 @@ QStringList BasePlugin::getInfo(){
 }
 QMap<QString,double> BasePlugin::getOptions(){
     return map;
+}
+
+void BasePlugin::findDir(QString _path){
+    this->path = &_path;
+    QString lib = "..\\lib\\";
+    QString defPath = lib + path;
+    directory = new QDir(*path);
+
+    if(!directory->exists()){
+        //We haven't been passed a complete path
+        //Attempt to find it
+        directory = new QDir(defPath);
+    }else if(!directory->exists()){
+        qWarning("Cannot find directory");
+    }
+}
+
+void BasePlugin::findJson(){
+    QString fileName = *path + ".json";
+    QStringList filters;
+    filters << fileName;
+    directory->setNameFilters(filters);
+
+    QStringList file;
+    if((file = directory->entryList(filters)).isEmpty()){
+        qWarning("Cannot find plugin");
+    }
+    doc = new QJsonDocument(QJsonDocument::fromJson(file.first().toLocal8Bit()));
+
+}
+
+//Extracts the plugin details and creates the objects to send to the user interface.
+void BasePlugin::extractJsonData(){
+    pluginMap = new QVariantMap(doc->object().toVariantMap());
+
+    QVariant var;
+    var = pluginMap->value("options");
+
+    QSequentialIterable itrVar = var.value<QSequentialIterable>();
+
+    //Assemble a map of Keys:Commands and a map of Keys:default values.
+    for(const QVariant &v : itrVar){
+       QMap<QString,QVariant> tempMap;
+       tempMap = v.toMap();
+       QString tempVal = tempMap.value("command").toString();
+       QMap<QString,double> tempKV = tempMap.value("pass").value<QMap<QString,double>>();
+       commandMap.insert(tempKV.firstKey(),tempVal);
+       map.unite(tempMap.value("pass").value<QMap<QString,double>>());
+    }
+
+}
+
+//Creates the QStringList of arguments which will be used to run the process.
+QStringList BasePlugin::uniteMaps(QMap<QString,double> usrOpts){
+    QStringList args;
+    QString comm;
+    double val;
+    QString argument;
+    for(auto k : usrOpts.keys()){
+        comm = commandMap.value(k);
+        if(usrOpts.value(k) != 0.0){
+            val = usrOpts.value(k);
+            args << comm + " " + val;
+        }else{
+            args << comm;
+        }
+    }
+    return args;
 }
 
 BasePlugin::~BasePlugin(){
--- BasePlugin.h
+++ BasePlugin.h
@@ -2,24 +2,44 @@
 #define BASEPLUGIN_H
 
 #include "IAlgorithm.h"
+#include <QDir>
 #include <QProcess>
 #include <QMap>
+#include <QJsonDocument>
+#include <QObject>
+#include <QString>
+#include <QJsonArray>
+#include <QVariant>
+#include <QTemporaryFile>
+#include <QTextStream>
 
 class BasePlugin : public IAlgorithm
 {
 public:
     BasePlugin();
-    BasePlugin(QJsonObject);
+    BasePlugin(QString);
     ~BasePlugin() override;
     QStringList getInfo() override;
     QString getStructure() override;
     QMap<QString,double> getOptions() override;
-    void initiate(QString filepath, QStringList opt) override;
+    QString initiate(QString filepath, QMap<QString, double> opt) override;
 
     //QProcess* proc;
 
 private:
+    void findJson();
+    void findDir(QString);
+    void extractJsonData();
+    QStringList uniteMaps(QMap<QString,double>);
     void buildOptions(QJsonObject);
+
+    QDir* directory;
+    QString* path;
+    QJsonDocument* doc;
+    QProcess* proc;
+
+    QMap<QString,QString> commandMap;
+
     QStringList info;
     QMap<QString, double> map;
     QString algName;
